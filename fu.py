c6ebf78fe9717e87072200e5ddb3921d33ee82a0
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-12-13 19:43:33 +0530

    Update fu.py

--- fu.py
+++ fu.py
@@ -12,9 +12,8 @@ def forward(client, message):
     words = ['dekho','TRUST','join','fix','😱','😳','👆','👇','☝️','https://','😂','🤔','pass','chase','link','suno','member','❓','loss','audio','open',"report",'paid','contact','baazigar','market','load','whatsapp','book','bhai','🐴','only','chut','tennis','teen','lavde','chutiya','bc','kya','line','LUND','WICKET LU','?','loda','telegram','chor',"kama","lakh",' id','स',"kitna"]
     for word in words:
      if word.casefold() in message.text.casefold():
-      f = True
-    if not f:
-      client.send_message(k,message.text)   
+      return
+     client.send_message(k,message.text)   
 
 @app.on_message(Filters.command("setf"))
 def forward(client, message):
