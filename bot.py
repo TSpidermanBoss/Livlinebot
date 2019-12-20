5b6cf5c14b0892dd4c5da674a939ae7bba59e4a0
TSpidermanBoss <49564169+TSpidermanBoss@users.noreply.github.com>
2019-12-20 12:32:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, Filters
-k = -1001350799105
+k = -1001212143796
 bot = "929464432:AAGcs59lrU1OKnSI7g02irSFQ4nB6b8XkKg"
 app = Client (session_name="rr",api_id=814511,api_hash="44462f0f278503255d5cc30941b617a9",bot_token = bot)                                   
 bullet = -1001428773103                                              
